56fe2ca146fbe23767c51afc7de2fdce346c92c2
Arkadiusz Sendecki <arek.sendecki@gmail.com>
2024-09-04 06:59:29 +0200

    Fast replace script

--- fast_replace.py
+++ fast_replace.py
@@ -1,7 +1,13 @@
 import os
 
-file_list = []
+file_paths_list = []
 for dirpath, dirnames, filenames in os.walk('C:\\Users\\areks\\PycharmProjects\\myfastapi\\docs'):
     for filename in filenames:
         if "extra-models" in filename:
-            file_list.append(os.path.join(dirpath, filename))
+            file_paths_list.append(os.path.join(dirpath, filename))
+
+for file_path in file_paths_list:
+    with open(file_path, 'r') as file:
+        text = file.read()
+    with open(file_path, 'w') as file:
+        file.write(text)
